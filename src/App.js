a13f457df5502577d1004dbe713b324e89b45e1e
Jinlin Xie <jinlinx@hotmail.com>
2020-09-15 23:08:35 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React , { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Propertylist from './components/propertylist';
 
 function App() {
+
+  const [showPage, setShowPage] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +13,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <input value="test"></input>
+        <button onClick={() => { 
+          setShowPage(1);
+        }}>Show Property List</button>
+        <button onClick={() => { 
+          setShowPage(2);
+        }}>Show User List</button>
+        test hellow
+        {showPage == 1 &&
+          <Propertylist />
+        }
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/propertylist.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function PropertyList() {
+    return <div>
+        List of properties
+    </div>
+}
+
+export default PropertyList;
