ae915a6d4e0e305a5306f1d5bdf260e11edbea28
Jinlin Xie <jinlinx@hotmail.com>
2020-09-28 23:38:49 -0400

    using new route

--- src/components/api.js
+++ src/components/api.js
@@ -1,10 +1,60 @@
 //import { get } from 'superagent';
 
-const getUrl =  sql => `http://192.168.1.119:8081/doQuery?sql=${sql}`;
+const apiBase = 'http://192.168.1.119:8081';
+const getUrl = sql => `${apiBase}/doQuery?sql=${sql}`;
 const request = require('superagent');
 const get = require('lodash/get');
 
 
-export async function getData(sql) {
-    return request.get(getUrl(sql)).send().then(r => get(r, 'body'));
+function doGetOp(url) {
+    return request.get(url).send().then(r => get(r, 'body'));
+}
+function doPostOp(url, data) {
+    return request.post(url).send(data).then(r => get(r, 'body'));
+}
+ async function getData(sql) {
+    return doGetOp(getUrl(sql));
+}
+
+async function getModel(name) {
+    return doGetOp(`${apiBase}/getModel?name=${name}`);
+}
+
+async function sqlGet(table, field, order) {
+    // "table":"tenantInfo",
+    // "field":["tenantID", "firstName"],
+    // "order":[{"name":"tenantID", "asc": true}, {"name":"firstName"}]
+    return doPostOp(`${apiBase}/sql/get`, {
+        table,
+        field,
+        order,
+    })
+}
+
+async function sqlAdd(table, fields, create) {
+//     "table":"tenantInfo",
+//     "fields":{"tenantID":"289a8120-01fd-11eb-8993-ab1bf8206feb", "firstName":"gang", "lastName":"testlong"},
+//    "create":true
+    //return id
+    return doPostOp(`${apiBase}/sql/create`, {
+        table,
+        fields,
+        create,
+    })
+}
+
+
+function sqlDelete(table, id) {
+    return doPostOp(`${apiBase}/sql/del`, {
+        table,id,
+    }) 
+}
+
+
+module.exports = {
+    getData,
+    getModel,
+    sqlGet,
+    sqlAdd,
+    sqlDelete,
 }
--- src/components/tenantlist.js
+++ src/components/tenantlist.js
@@ -1,26 +1,28 @@
 import React, {useState, useEffect} from 'react';
-import { getData } from './api';
+import { getData, getModel, sqlGet, sqlAdd, sqlDelete } from './api';
 import GenCrud from './GenCrud';
+import mod from './models';
 
 function TenantList() {
-    const columnInf = [
-        { field: 'tenantID', desc: 'Id', type: 'uuid', required: true, isId: true },
-        { field: 'firstName', desc: 'First Name', required: true },
-        { field: 'lastName', desc: 'Last Name', require: true },
-        { field: 'email', desc: 'Email', },
-        { field: 'phone', desc: 'Phone', },
-        { field: 'ssn', desc: 'SSN', },
-        { field: 'driverID', desc: 'Driver License', },
-        { field: 'driverIDState', desc: 'State', },
-        { field: 'momName', desc: 'Mother', },
-        { field: 'momPhone', desc: 'Mom\'s phone number', },
-        { field: 'dadName', desc: 'Dad Name', },
-        { field: 'dadPhone', desc: 'Dad Phone', },
-    ];
+    // [
+    //     { field: 'tenantID', desc: 'Id', type: 'uuid', required: true, isId: true },
+    //     { field: 'firstName', desc: 'First Name', required: true },
+    //     { field: 'lastName', desc: 'Last Name', require: true },
+    //     { field: 'email', desc: 'Email', },
+    //     { field: 'phone', desc: 'Phone', },
+    //     { field: 'ssn', desc: 'SSN', },
+    //     { field: 'driverID', desc: 'Driver License', },
+    //     { field: 'driverIDState', desc: 'State', },
+    //     { field: 'momName', desc: 'Mother', },
+    //     { field: 'momPhone', desc: 'Mom\'s phone number', },
+    //     { field: 'dadName', desc: 'Dad Name', },
+    //     { field: 'dadPhone', desc: 'Dad Phone', },
+    // ];
     const [tenants, setTenants] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [columnInf, setColumnInf] = useState([]);
     const reload = () => {
-        getData('select * from tenantInfo').then(res => {
+        sqlGet('tenantInfo', columnInf.map(f=>f.field)).then(res => {
             setTenants(res);
             setLoading(false);
         });
@@ -28,13 +30,25 @@ function TenantList() {
     
 
     useEffect(() => {
-        reload();
+        const ld = async ()=>{
+            if (!mod.models.tenantInfo) {
+                mod.models.tenantInfo = await getModel('tenantInfo');
+            }
+            console.log(mod);
+            setColumnInf(mod.models.tenantInfo.fields);
+            reload();
+        }
+        ld();
     }, []);
 
     const doAdd = data => {
-        const sql = `insert into tenantInfo (${columnInf.map(c => `\`${c.field}\``).join(',')}) values (${columnInf.map(f => data[f.field] || '').map(v => `'${v}'`).join(',')
-            })`;
-        getData(sql).then(() => {
+        //const sql = `insert into tenantInfo (${columnInf.map(c => `\`${c.field}\``).join(',')}) values (${columnInf.map(f => data[f.field] || '').map(v => `'${v}'`).join(',')
+        //    })`;
+        const submitData = columnInf.reduce((acc, f) => {
+            acc[f.field] = data[f.field];
+            return acc;
+        }, {});
+        sqlAdd('tenantInfo',submitData, true).then(() => {
             setLoading(true);
             console.log('reloading');
             reload();
@@ -46,14 +60,14 @@ function TenantList() {
 
     const doDelete = (name,id) => {
         setLoading(true);
-        getData(`delete from tenantInfo where ${name}='${id}'`).then(() => {
+        sqlDelete('tenantInfo', id).then(() => {
             reload();
-        });
+        })
     }
     return <div>
-        <p class='subHeader'>List of Tenantsaaaaa</p>
+        <p class='subHeader'>List of Tenants</p>
         {
-            loading ? <p>Loading</p> :
+            (loading || !columnInf) ? <p>Loading</p> :
                 <div>
                     <GenCrud
                         displayFields={['tenantID', 'firstName', 'lastName']}
