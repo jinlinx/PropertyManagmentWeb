99c7969d99329e7812b7d9e99d3016569e295941
Jinlin Xie <jinlinx@hotmail.com>
2020-09-30 20:48:52 -0400

    adding lease

--- src/components/leaselist.js
+++ src/components/leaselist.js
@@ -2,7 +2,45 @@ import React  from 'react';
 import GenList from './GenList';
 
 function LeaseList() {  
-    return <GenList table={'leaseInfo'} title={'Lease List'} /> 
+    return <GenList table={'leaseInfo'}
+    processForeignKey={
+        (fk, datas) => {
+            return datas.map(data => {
+                return {
+                    value: data[fk.field],
+                    label: data['address'] + ' ' + data['ownerID'],
+            }
+        })
+    }
+}
+displayFields={
+        //actualy don't need to do this
+        [
+            { field: 'deposit', desc: 'Deposit',  },
+            { field: 'endDate', desc: 'End Date', },
+            { field: 'startDate', desc: 'Start Date',  },
+            { field: 'houseAddress', desc: 'House'},
+            { field: 'comment', desc: 'Comment' },
+            { field: 'monthlyRent', desc: 'Monthly Rent' },
+            //{field: 'ownerID',desc: 'Owner ID',require: true,foreignKey: {table: 'ownerInfo',field: 'ownerID'}},
+        ]
+    }
+    loadMapper={
+        (type,fields) => {
+            if(type==='fields') {
+                return fields;
+            }
+            if(type==='joins') {
+                return {
+                    'houseInfo': {
+                        address: 'houseAddress',
+                        ownerID: 'ownerID',
+                    }
+                }
+            }
+        }
+    }
+        title={'Lease List'} /> 
 }
 
 export default LeaseList;
