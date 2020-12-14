39ad404c6bda6c670d6283e416bf699e5d5ffad7
Gang Zhang <gzhangx@hotmail.com>
2020-12-14 12:51:36 -0500

    temp

--- src/components/paymentMatch/TenantMatcher.js
+++ src/components/paymentMatch/TenantMatcher.js
@@ -7,26 +7,20 @@ import { GetOrCreate } from './GetOrCreate';
 import { sqlFreeForm } from '../api';
 import {
     saveTenantProcessorPayeeMapping,
-    getHouses,
 } from '../aapi';
 import { nameToFirstLast } from '../util';
-import { add, get } from 'lodash';
+import { add, get, set } from 'lodash';
+import GenCrudAdd from '../GenCrudAdd';
+import { createHelper } from '../datahelper';
 export function TenantMatcher(props) {
 
     const { onClose, name, source } = props.context;
     const show = !!name;
     //const { show } = props;
     const [curTenantSelection, setCurTenantSelection] = useState({});
-    const [curHouseSelection, setCurHouseSelection] = useState({});
     const [showProgress, setShowProgress] = useState(false);
-    const firstLast = nameToFirstLast(name || '');
-    const [tenantName, setTenantName] = useState(firstLast.firstName);
-    useEffect(() => {
-        setTenantName({
-            firstName: firstLast.firstName || '',
-            lastName: firstLast.lastName || '',
-        });
-    }, [firstLast.firstName, firstLast.lastName]);
+    const [columnInfo, setColumnInfo] = useState([]);
+    const [isCreateNew, setIsCreateNew] = useState(false);
     const loadTenantOptions = async (name = '') => {
         const { firstName, lastName } = nameToFirstLast(name || '');
         const res = await sqlFreeForm(`select tenantID, firstName, lastName from tenantInfo 
@@ -38,23 +32,32 @@ export function TenantMatcher(props) {
         return fm;
     };
 
-    const loadHouseOptions = async (address = '') => {
-        const res = await getHouses(address);
-        const fm = res.map(r => ({
-            label: `${r.address} ${r.city} ${r.state}`,
-            value: r,
-        }));
-        return fm;
-    };
     const tenantID = get(curTenantSelection, 'value.tenantID');
     const mapToLabel = curTenantSelection.label;
-    console.log(curTenantSelection);
+    
+    useEffect(() => {
+        const helper = createHelper('tenantInfo');
+        
+        helper.loadModel().then(() => {
+            const columnInfo = helper.getModelFields();
+            setColumnInfo(columnInfo);
+        });
+    }, []);
     return <div >
         <Modal show={!!showProgress}>
             <Container>
                 {showProgress}
             </Container>
         </Modal>
+        <GenCrudAdd columnInfo={columnInfo} show={isCreateNew}
+            onOK={added => {
+                if (added) {
+
+                } else {
+                    setIsCreateNew(false);
+                }
+            }}
+        ></GenCrudAdd>
         <Modal show={show}>
             <Modal.Header closeButton onClick={() => {
                 onClose();
@@ -64,10 +67,7 @@ export function TenantMatcher(props) {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body className="show-grid">
-                <Container>
-                    <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                        <Tab eventKey="LinkExisting" title="Link Existing">                            
-                            <Container>
+                <Container>                    
                                 <Row><Col>Ma: {mapToLabel} to {name} {source} {tenantID}</Col></Row>
                                 <Row>
                                 <Col xs={12} md={8}>
@@ -75,7 +75,12 @@ export function TenantMatcher(props) {
                                         curSelection: curTenantSelection, setCurSelection: setCurTenantSelection,
                                         loadOptions: loadTenantOptions,
                                     }}></GetOrCreate>                                    
-                                </Col>
+                        </Col>
+                        <Col>
+                            <Button onClick={() => {
+                                setIsCreateNew(true);
+                            }}>Create New</Button>
+                        </Col>
                                 <Col>
                                         <Button disabled={!!showProgress} onClick={() => {                                            
                                             if (!tenantID) {
@@ -93,38 +98,7 @@ export function TenantMatcher(props) {
                                     }}>Link</Button>
                                     </Col>
                                 </Row>
-                            </Container>                            
-                        </Tab>
-                        <Tab eventKey="new" title="Create New">
-                            <Row>
-                                <Col>FirstName</Col>
-                                <Col>< Form.Control as="input" value={tenantName.firstName} name="tenantFirstName" onChange={e => {                                    
-                                    setTenantName({
-                                        ...tenantName,
-                                        firstName: e.target.value,
-                                    })
-                                }} /></Col>                                
-                            </Row>
-                            <Row>          
-                                <Col>LastName</Col>
-                                <Col>< Form.Control as="input" value={tenantName.lastName} name="tenantLastName" onChange={e => {
-                                    setTenantName({
-                                        ...tenantName,
-                                        lastName: e.target.value,
-                                    })
-                                }} /></Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                    <GetOrCreate context={{
-                                        curSelection: curHouseSelection, setCurSelection: setCurHouseSelection,
-                                        loadOptions: loadHouseOptions,
-                                    }}></GetOrCreate>
-                                </Col>
-                            </Row>
-                        </Tab>                        
-                    </Tabs>                    
-                </Container>
+                            </Container>
             </Modal.Body>
             <Modal.Footer>
                 <Button  onClick={
