0dbb6dfc8c54813abb5ae54d1f5328fb0b80188a
Gang Zhang <gzhangx@hotmail.com>
2020-12-19 00:29:11 -0500

    fix timer

--- src/components/Developer.js
+++ src/components/Developer.js
@@ -10,12 +10,31 @@ import { sqlFreeForm, getData } from './api';
 function Developer(props) {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
+    const [timerId, setTimerId] = useState(0);
+    
+    const pullStatementMsg = () => {
+        const tfunc = () => {
+            getData('misc/getStatementProcessingMsg').then(msg => {
+                setMessage(msg.message);
+                console.log('timerid is = '+timerId);
+                //if (timerId) {
+                    const hndl = setTimeout(tfunc, 100);
+                    setTimerId(hndl);
+                //}
+            });
+
+        };
+        const hndl = setTimeout(tfunc, 100);
+        console.log('start time=' + hndl);
+        setTimerId(hndl);
+    }
     const importPayment = who => {
         setMessage('Please wait');
         getData(`misc/statement?who=${who}`).then(res => {
             setMessage('import done');
             setMessage(JSON.stringify(res));
         });
+        pullStatementMsg();
     }
     return <Container>
         <Row>
@@ -35,6 +54,17 @@ function Developer(props) {
                     messages.map(m => <Alert variant="secondary">{m}</Alert> )
                 }                
             </Col>
+        </Row>
+        <Row>
+            <Col><Button onClick={() => {
+                if (timerId) {
+                    console.log('stop timer ' + timerId);
+                    clearTimeout(timerId);
+                    setTimerId(0);
+                } else {
+                    pullStatementMsg();
+                }
+            }}>{timerId ? 'Stop Timer' : 'Start Timer'}</Button></Col>
         </Row>
         <Row>
             <Col>
