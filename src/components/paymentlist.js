0df73ab6cb4723147fd8f16a2f143dda62bf7341
Gang Zhang <gzhangx@hotmail.com>
2021-03-17 23:27:51 -0400

    adding emails dialog

--- src/components/paymentlist.js
+++ src/components/paymentlist.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import GenList from './GenList';
 import { fmtDate } from './util';
-
+import {Modal, Button} from 'react-bootstrap';
+import {sqlGet} from './api';
 function PaymentList(props) {
-    return <GenList 
+
+    const [pageState, setPageState] = useState({
+        retrivingData: 0,
+        operationText: '',
+        leaseIDToSearch: null,
+        tenants:[],
+    });
+
+    useEffect(()=>{
+        if (!pageState.leaseIDToSearch) return;
+        sqlGet({
+            table:'leaseTenantInfo',
+            whereArray:[{field:'leaseID', val:pageState.leaseIDToSearch, op:'='}]            
+        }).then(res=>{
+            console.log(res);
+            setPageState(state=>({
+                ...state,
+                leaseIDToSearch: null,
+                retrivingData: state.retrivingData-1,
+                operationText: ``,
+                tenants: res.rows,
+            }));
+        })
+    },[pageState.leaseIDToSearch]);
+    const emailClick = (name, row)=>{
+        //console.log('val is onemail click ' + val);
+        console.log(row.leaseID);
+        setPageState(state=>({
+            ...state,
+            tenants:[],
+            leaseIDToSearch: row.leaseID,
+            retrivingData: state.retrivingData+2,
+            operationText: `Getting email list for ${name}`,
+        }));
+    };
+    const handleClose = ()=>{
+        setPageState(state=>({
+            ...state,
+            retrivingData: state.retrivingData-1,
+        }));
+    };
+    return <>
+    <Modal show={!!pageState.retrivingData} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Please Wait</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <div>{pageState.operationText}</div>
+            <ul>
+            {                
+                pageState.tenants.map(tenant=>{
+                    return <li>{tenant.firstName} {tenant.lastName} email={tenant.email}</li>
+                })
+            }
+            </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>          
+        </Modal.Footer>
+      </Modal>
+    <GenList 
     {...props}
     table={'rentPaymentInfo'}    
         displayFields={
@@ -13,7 +76,9 @@ function PaymentList(props) {
                     field: 'receivedDate', desc: 'Date Received', dspFunc: fmtDate
                 },
                 {field: 'receivedAmount',desc: 'Received Amount',},
-                { field: 'paidBy', desc: 'Paid By', },
+                { field: 'paidBy', desc: 'Paid By',  dspFunc: (name, row)=>{
+                    return <button type="button" className="btn btn-link" onClick={()=>emailClick(name, row)}>{name}</button>;
+                }},
                 //{field: 'paymentType',desc: 'Payment Type',},
                 //{ field: 'comment', desc: 'Lease' },
                 { field: 'address', desc: 'Address' },
@@ -22,7 +87,7 @@ function PaymentList(props) {
             ]
         }
         
-        title={'Payment List'} />
+        title={'Payment List'} /></>
 }
 
 export default PaymentList;
