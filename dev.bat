470d8716b6dae1a8e484fdd6048a50c44e3b4f7f
Gang Zhang <gzhangx@hotmail.com>
2021-05-31 11:52:43 -0400

    dev

---
+++ dev.bat
@@ -0,0 +1,1 @@
+set "REACT_APP_ENDPOINT_ENV=dev" && npm start
--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,8 @@
 //import { get } from 'superagent';
 //const urlBase = 'http://localhost:8081';
-const urlBase = process.env.NODE_ENV === 'dev' ? 'http://localhost:8081': 'http://192.168.1.41';
+console.log(process.env.REACT_APP_ENDPOINT_ENV)
+console.log('envvvvvv')
+const urlBase = process.env.REACT_APP_ENDPOINT_ENV === 'dev' ? 'http://localhost:8081': 'http://192.168.1.41';
 const apiBase=`${urlBase}/pmapi`;
 const getUrl=path => `${apiBase}/${path}`;
 const request = require('superagent');
