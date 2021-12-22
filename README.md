24d1ef2d925e58ac41791d82660431d288fcd670
Gang Zhang <gzhangx@hotmail.com>
2021-12-22 18:39:20 -0500

    refactoring

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 
 
 https://developers.google.com/oauthplayground 
+
+https://bootstrapious.com/p/bootstrap-sidebar
+
+https://bootstrapious.com/tutorial/sidebar/index.html
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import './bs.css';
 import Propertylist from './components/propertylist';
 import Leaselist from './components/leaselist';
 import WorkerList from './components/workerList';
@@ -13,6 +14,7 @@ import Developer from './components/Developer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import HomePage from './components/HomePage';
+import HomePageNew from './HomePageNew';
 import { getOwners } from './components/aapi';
 import LeftMenu from './components/leftMenu';
 
@@ -62,7 +64,7 @@ function App() {
       setShowPage(who + from);
     }}>{page.desc}</button></td>
   })
-  return <HomePage />;
+  return <HomePageNew />;
   return (
     <div className="App" >
       <table>
